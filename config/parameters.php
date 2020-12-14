dbf6f5d5d7f02753e02381c7e2efbe6870605e95
techmindconsulting <techmind.consulting.sasu@gmail.com>
2020-12-14 03:45:18 +0100

    udpate arbo

--- config/parameters.php
+++ config/parameters.php
@@ -8,6 +8,10 @@
     define("WHATSAPP_API", "https://api.whatsapp.com/send");
     define("WHATSAPP_API_PARAM_PHONE", "+33611571631");
     define("WHATSAPP_API_PARAM_MESSAGE", "Bonjour");
+
+    define("GOOGLE_MAPS_URL", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4990.816503056546!2d5.391916738540558!3d43.29397768091119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0a74135d299%3A0x83457b094bcf0c4b!2s35%20Rue%20Vitalis%2C%2013005%20Marseille!5e0!3m2!1sen!2sfr!4v1602067173306!5m2!1sen!2sfr");
+    define("GOOGLE_MAPS_WIDTH", 600);
+    define("GOOGLE_MAPS_HEIGHT", 450);
     
     define("DATABASE_USER", "root");
     define("DATABASE_PASSWORD", "");
---
+++ includes/section/map.php
@@ -0,0 +1,14 @@
+<section id="localisation" class="wrapper">
+    <div class="container">
+        <h2>Plan d'acces</h2>
+        <p><?= CONTACT_ADDRESS ?></p>
+        <p><?= CONTACT_PHONE ?></p>
+        <iframe 
+        src="<?= GOOGLE_MAPS_URL ?>"  
+        width="<?= GOOGLE_MAPS_WIDTH ?>" 
+        height="<?= GOOGLE_MAPS_HEIGHT ?>"
+        allowfullscreen="" 
+        aria-hidden="false" 
+        tabindex="0"></iframe>
+    </div>
+</section>
