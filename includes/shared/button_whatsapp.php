b792d2ff4088cda3c0993ca8ae21be66c5948c61
techmindconsulting <techmind.consulting.sasu@gmail.com>
2020-12-14 23:54:04 +0100

    fix url whatsapp button

--- includes/shared/button_whatsapp.php
+++ includes/shared/button_whatsapp.php
@@ -1,5 +1,5 @@
 <div class="whatsapp-button">
     <a href="<?= WHATSAPP_API . '?phone='. WHATSAPP_API_PARAM_PHONE . '&text='. WHATSAPP_API_PARAM_MESSAGE ?>">
-        <img src="images/whatsapp.png" alt="Contactez nous sur WhatsApp" width="70">
+        <img src="assets/images/whatsapp.png" alt="Contactez nous sur WhatsApp" width="70">
     </a>
 </div>
